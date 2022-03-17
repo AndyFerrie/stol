2543e5435d4af0a7b8a8c5766f74929bed8aaf0e
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-03-17 23:05:19 +0000

    Add findId function to ChairDetail

--- src/components/chairs/ChairDetail.jsx
+++ src/components/chairs/ChairDetail.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import chairData from '../../db.json'
 
 const ChairDetail = () => {
-   const id = window.location.pathname.substring(1);
+
+   const chairID = window.location.pathname.substring(1);
+
+   function findId(chairData, idToLookFor) {
+    var chairArray = chairData.results;
+    for (var i = 0; i < chairArray.length; i++) {
+        if (chairArray[i].id === idToLookFor) {
+            return(chairArray[i]);
+        }
+    }
+}
+
+var item = findId(chairData, chairID);
 
    return (
-    <div></div>
+       <>
+        <div>{item.productName}</div>
+        <div>{item.price}</div>
+       </>
+    
   )
 }
 
